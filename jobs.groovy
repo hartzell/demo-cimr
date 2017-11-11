ce7143eb6a491b09518a632fc9e9d775f6ffa2b8
hartzell <hartzell@alerce.com>
2017-11-11 22:39:45 +0000

    Delete first job

--- jobs.groovy
+++ jobs.groovy
@@ -1,12 +1,4 @@
 // a simple little jenkins job, proof of concept
-job('my-first-job') {
-    steps {
-        shell """#!/bin/bash
-echo "Hello world!"
-"""
-    }
-}
-// a simple little jenkins job, proof of concept
 job('my-second-job') {
     steps {
         shell """#!/bin/bash
