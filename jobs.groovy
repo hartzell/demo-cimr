212af69e5fedb9be6e76060f71c9cb0a01cd8705
George Hartzell <hartzell@alerce.com>
2017-11-11 20:16:01 +0000

    bloop

--- jobs.groovy
+++ jobs.groovy
@@ -6,12 +6,3 @@ echo "Hello world!"
 """
     }
 }
-// a simple little jenkins job, proof of concept
-job('my-second-job') {
-    steps {
-        shell """#!/bin/bash
-echo "Hello world!"
-"""
-    }
-}
-
