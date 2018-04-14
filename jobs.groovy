8d715e999bfb6b642e0f9b82ccccc952d14318b1
George Hartzell <hartzell@alerce.com>
2018-04-14 20:54:20 +0000

    Delete second job, add PR job

--- jobs.groovy
+++ jobs.groovy
@@ -6,10 +6,50 @@ echo "Hello world!"
 """
     }
 }
-job('my-second-job') {
-    steps {
-        shell """#!/bin/bash
-echo "Hello world!"
-"""
+
+// This job will fire whenever a Pull Request is created and/or whenever
+// one of the trigger phrases occurs in the PR's comments
+// (e.g. OK to test).
+// There's a bit of magic here, check out the Github Pull Request Builder
+// plugin docs...
+job('pull-request-to-demo-cimr') {
+  scm {
+    git {
+      remote {
+        // It's tempting to use the `github()` helper here, but that
+        // way lies madness (it assumes `https://`, sigh...).
+        url('git@github.com:hartzell/demo-cimr')
+        credentials('git-key')
+        refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+      }
+      branch('${sha1}')
+      extensions {
+        wipeOutWorkspace()
+      }
     }
+    properties {
+      githubProjectUrl('https://github.com/hartzell/demo-cimr')
+    }
+  }
+  triggers {
+    githubPullRequest {
+      admins(['hartzell'])
+      useGitHubHooks()
+      whiteListTargetBranches(['develop'])
+      allowMembersOfWhitelistedOrgsAsAdmin()
+      extensions {
+        commitStatus {
+          context('Pull Request tests demo')
+        }
+      }
+    }
+  }
+  steps {
+    shell '''#!/bin/sh
+sleep 42
+echo "Hello world!"
+true
+'''
+  }
 }
+
